464cbc1b8d6af9e6370ec40932022c26d33846e0
Nabil <abubakar.nabil.210@gmail.com>
2026-01-06 22:59:56 +0100

    feat: made a network_health script to ping ipaddresses to see if the user has network

---
+++ scripts/network_health.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ip_list=("1.1.1.1" "9.9.9.9" "149.112.112.112" "208.67.222.222")   
+
+
+# --- Logic --- 
+echo "Checking network Connectivity ....."
+
+for i in "${ip_list[@]}";do
+    echo "$i"
+    ping -c 4 "$i"
+    echo "pinged ip addresses to check internet connection"
+done
+
